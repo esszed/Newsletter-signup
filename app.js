dc303e357e2055a79efdf067c8c5cda61b15140e
Štěpán Zoubek <stepan.zoubek@gmail.com>
2020-07-22 15:09:16 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/signup.html`)
+})
+
+app.post('/', (req, res) => {
+  let firstName = req.body.fName
+  let lastName = req.body.lName
+  let email = req.body.mail
+  let data = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscribed',
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  }
+  let jsonData = JSON.stringify(data)
+  const options = {
+    url: 'https://us20.api.mailchimp.com/3.0/lists/dc8d4559fd',
+    method: 'POST',
+    headers: {
+      Authorization: 'stepan 36804bdb7e2a951c50e95126fc00b95a-us20'
+    },
+    body: jsonData
+  }
+
+  request(options, (error, response, body) => {
+    if (error) {
+      console.log(error)
+      res.sendFile(`${__dirname}/failure.html`)
+    } else {
+      if (response.statusCode == 200) {
+        res.sendFile(`${__dirname}/success.html`)
+      } else {
+        res.sendFile(`${__dirname}/failure.html`)
+      }
+    }
+  })
+})
+
+app.post('/failure', (req, res) => {
+  res.redirect(`/`)
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running on port 3000')
+})
+
+// 36804bdb7e2a951c50e95126fc00b95a-
+
+// dc8d4559fd
